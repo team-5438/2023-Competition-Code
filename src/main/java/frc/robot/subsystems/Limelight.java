dcc55c7ca9403618251d0f7a4a19a3b96324f457
Phantomforce260 <120600016+Phantomforce260@users.noreply.github.com>
2023-01-30 21:14:13 +0000

    added imports

--- src/main/java/frc/robot/subsystems/Limelight.java
+++ src/main/java/frc/robot/subsystems/Limelight.java
@@ -2,6 +2,14 @@ package frc.robot.subsystems;
 
 import frc.robot.Constants;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+
+public NetworkTable table;
+NetworkTableEntry tx;
+NetworkTableEntry ty;
+NetworkTableEntry ta;
 
 public class Limelight {
 
