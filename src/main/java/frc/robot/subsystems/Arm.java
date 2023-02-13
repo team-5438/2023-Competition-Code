d4e3e05f10be35b686197743668c1dc16239fb9d
Samuel <samgleonte@gmail.com>
2023-02-13 16:19:42 -0500

    Fixing subsystem error

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -3,11 +3,13 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.math.MathUtil;
 import frc.robot.Constants;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
 public class Arm extends SubsystemBase {
 	private final CANSparkMax pivotMotor = new CANSparkMax(Constants.PIVOT_MOTOR_SPARKMAX_ID, MotorType.kBrushless);
-	public void pivotArm(speed) {
+	public void pivotArm(double speed) {
 		// 1 = forward, -1 = backwards
-		pivotMotor.set(applyDeadband(speed, 0.02));
+		//pivotMotor.set(applyDeadband(speed, 0.02));
 	}
 }
 
