56999b50564cd13ef3ccf3874963f2d3d78ca0b5
Slys <slysdeveloper@gmail.com>
2023-02-14 15:30:29 -0500

    Added limit switch code (needs testing)

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -5,12 +5,23 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.math.MathUtil;
 import frc.robot.Constants;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.*;
 
 public class Arm extends SubsystemBase {
-	private final CANSparkMax pivotMotor = new CANSparkMax(Constants.PIVOT_MOTOR_SPARKMAX_ID, MotorType.kBrushless);
+	static private final CANSparkMax pivotMotor = new CANSparkMax(Constants.PIVOT_MOTOR_SPARKMAX_ID,
+			MotorType.kBrushless);
+	static private DigitalInput topLimitSwitch = new DigitalInput(0);
+	static private DigitalInput bottomLimitSwitch = new DigitalInput(0);
 
 	static public void pivotArm(double speed) {
+		if (speed > 0 && topLimitSwitch.get()) {
+			return;
+		} else if (speed < 0 && bottomLimitSwitch.get()) {
+			return;
+		} else {
+			pivotMotor.set(MathUtil.applyDeadband(speed, 0.02));
+
+		}
 		// 1 = forward, -1 = backwards
-		// pivotMotor.set(applyDeadband(speed, 0.02));
 	}
 }
